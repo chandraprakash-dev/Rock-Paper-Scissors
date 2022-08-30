2e29d4147be8c23185b390f24943cf657eee61ed
Chandra Prakash <pr.chandraprakash@gmail.com>
2022-08-30 13:31:56 +0530

    Write function to play round on player selection

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,23 @@
 /************************************************************************************/
-objects = [
+const objects = [
   { name: "rock", action: "crushes", beats: "scissors" },
   { name: "paper", action: "covers", beats: "rock" },
   { name: "scissors", action: "cuts", beats: "paper" },
 ];
 
-// let playerScore = 0;
-// let computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 // let tieScore = 0;
 // let roundNo = 1;
 //
 // const roundNoField = document.querySelector('.summary-card__round');
-// const playerSelectionImage = document.querySelector('.player-card__img__img');
-// const computerSelectionImage = document.querySelector('.computer-card__img__img');
-// const playerScoreField = document.querySelector('.player-card__score');
-// const computerScoreField = document.querySelector('.computer-card__score');
+// const playerSelectionImage = document.querySelector(".player-card__img__img");
+// const computerSelectionImage = document.querySelector(".computer-card__img__img" );
+// const playerScoreField = document.querySelector(".player-card__score");
+// const computerScoreField = document.querySelector(".computer-card__score");
 //
 // const finalResults = document.querySelector('.banner');
-// const roundResultsField = document.querySelector('.summary-card__info');
+// const roundResultsField = document.querySelector(".summary-card__info");
 //
 // /************************************************************************************/
 //
@@ -28,9 +28,9 @@ objects = [
 // }
 
 // function updateSelections(playerSelection, computerSelection) {
-//   roundNoField.textContent = `${roundNo++}`;
-//   playerSelectionImage.src = `./assets/images/${playerSelection}.png`;
-//   computerSelectionImage.src = `./assets/images/${computerSelection}.png`;
+//   // roundNoField.textContent = `${roundNo++}`;
+//   playerSelectionImage.src = `./assets/images/${playerSelection.name}.png`;
+//   computerSelectionImage.src = `./assets/images/${computerSelection.name}.png`;
 // }
 //
 //
@@ -50,9 +50,9 @@ objects = [
 //
 //   roundNoField.textContent = roundNo;
 //
-//     playerScoreField.textContent = playerScore;
-//   computerScoreField.textContent = computerScore;
-//   roundResultsField.textContent = 'Let\'s begin';
+// playerScoreField.textContent = playerScore;
+// computerScoreField.textContent = computerScore;
+// roundResultsField.textContent = "Let's begin";
 // }
 //
 // function resetFinalResults() {
@@ -69,46 +69,11 @@ objects = [
 //     }
 // }
 //
-// function updateResults(playerSelection, computerSelection, winner, msg) {
-//     let winnerScore;
-//     if (winner === "computer") {
-//         winnerScore = computerScore;
-//     } else if (winner === "player") {
-//         winnerScore = playerScore;
-//     }
-//
-//     console.log('winner is ' + winner);
-//     if (winner !== 'tie') {
-//         const winnerField = document.querySelector(`.${winner}-card__score`);
-//         winnerField.textContent = winnerScore;
-//     }
-//
-//     roundResultsField.textContent = msg;
-// }
-
-// function computeResults(playerSelection, computerSelection) {
-//   let msg, winner;
-//   if (playerSelection === computerSelection) {
-//     playSound('tie');
-//     msg = `This round is a draw ${playerSelection} cant beat ${computerSelection}. `;
-//     winner = "tie";
-//     tieScore++;
-//   } else if (itemDefeats[playerSelection] === computerSelection) {
-//     playSound(playerSelection);
-//     msg = `You win this round! ${playerSelection} beats ${computerSelection}. `;
-//     winner = "player";
-//     playerScore++;
-//   } else if (itemDefeats[computerSelection] === playerSelection) {
-//     playSound(computerSelection);
-//     msg = `Computer wins this round! ${computerSelection} beats ${playerSelection}. `;
-//     winner = "computer"
-//     computerScore++;
-//   }
-//
-//   console.log(`Player: ${playerSelection}, ${playerScore}`);
-//   console.log(`Computer: ${computerSelection}, ${computerScore}`);
-//
-//   return {winner, msg};
+// function updateResults(result) {
+//   console.log(result);
+//   roundResultsField.textContent = result;
+//   playerScoreField.textContent = playerScore;
+//   computerScoreField.textContent = computerScore;
 // }
 
 function getComputerChoice() {
@@ -127,7 +92,7 @@ function getGameResult(playerScore, computerScore) {
   }
 }
 
-function playRound(playerSelection, computerSelection) {
+function getRoundResult(playerSelection, computerSelection) {
   if (playerSelection.name === computerSelection.name) return "It is a tie";
   if (playerSelection.beats === computerSelection.name) {
     return `You win this round! ${playerSelection.name} ${playerSelection.action} ${computerSelection.name}`;
@@ -160,24 +125,28 @@ function game() {
   console.log(gameResult);
 }
 
-// function playRound(playerSelection) {
-//   if (playerScore === 5 || computerScore === 5) {
-//       wantsToPlayAgain();
-//   }
-//   // functionality to check who wins the round
-//   const computerSelection = getComputerChoice();
+function playRound(playerSelection) {
+  // if (playerScore === 5 || computerScore === 5) {
+  //     wantsToPlayAgain();
+  // }
+  // functionality to check who wins the round
+  const computerSelection = getComputerChoice();
+  console.log(playerSelection, computerSelection);
+  // updateSelections(playerSelection, computerSelection);
 
-//   updateSelections(playerSelection, computerSelection);
-//   console.log(playerSelection, computerSelection);
-//   let {winner, msg} = computeResults(playerSelection, computerSelection);
-//   updateResults(playerSelection, computerSelection, winner, msg);
-//
-//   // Reset the scores once one of the players wins 5 points
-//   if (playerScore === 5 || computerScore === 5) {
-//     winner = playerScore === 5 ? 'player' : 'computer';
-//     updateFinalResults(winner);
-//   }
-// }
+  const result = getRoundResult(playerSelection, computerSelection);
+
+  if (result.includes("win")) playerScore++;
+  if (result.includes("lose")) computerScore++;
+
+  console.log(result);
+  // updateResults(result);
+  // // Reset the scores once one of the players wins 5 points
+  // if (playerScore === 5 || computerScore === 5) {
+  //   winner = playerScore === 5 ? "player" : "computer";
+  //   updateFinalResults(winner);
+  // }
+}
 //
 // function playSound(sound) {
 //   const audio = document.querySelector(`.${sound}-sound`);
@@ -193,6 +162,7 @@ playerOptions.addEventListener("click", (e) => {
   console.log(e.target);
   if (!optionDiv.classList.contains("card__option")) return;
   // playSound("click");
-  const object = optionDiv.getAttribute("data-option");
+  const option = optionDiv.getAttribute("data-option");
+  const object = objects[option];
   playRound(object);
 });
